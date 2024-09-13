06480f0beb9601ef7540e680a83b9362ac4527b8
Hwang-jun-woo <miend1234@gmail.com>
2024-09-13 13:20:59 +0900

    commit massage

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,10 @@
 def print_hello():
-    print("Hello World!!")
+    for i in range(10):
+        if i < 1:
+            print("Hello, world!")
+        else:
+            print("Hello, world again!")        
+
 
 if __name__ == '__main__':
     print_hello()
